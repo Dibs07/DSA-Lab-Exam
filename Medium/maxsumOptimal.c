49bb5f8ce8672d7e76fd8fdb7486f70f7fcbef2a
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-02-03 22:12:19 +0530

    new file:   Medium/maxsumOptimal.c

---
+++ Medium/maxsumOptimal.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//Kadane's Algorithm
+void main(){
+    int n;
+    printf("Enter the number of elements you want in array: ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    int max=arr[0],sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        if(sum>max){
+            max=sum;
+        }
+        if(sum<0){
+            sum=0;
+        }
+    }
+    printf("Max sum subarray is %d",max);
+}
