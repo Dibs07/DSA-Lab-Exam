f0cf46d34a8c3c7dfae50f7e967f177184d2ca90
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-02-03 22:30:16 +0530

    new file:   Medium/triangular.c

---
+++ Medium/triangular.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main()
+{
+    int m, n;
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d", &m, &n);
+    if (m == n)
+    {
+        int arr[m][n];
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                scanf("%d", &arr[i][j]);
+            }
+        }
+        printf("Matrix is:\n");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                printf("%d", arr[i][j]);
+            }
+            printf("\n");
+        }
+        //Upper Triangular
+        printf("The upper triangular matrix is:\n");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i < j)
+                {
+                    printf("0 ");
+                }
+                else
+                {
+                    printf("%d ", arr[i][j]);
+                }
+            }
+            printf("\n");
+        }
+        //Lower Triangular
+        printf("The lower triangular matrix is:\n");
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i > j)
+                {
+                    printf("0 ");
+                }
+                else
+                {
+                    printf("%d ", arr[i][j]);
+                }
+            }
+            printf("\n");
+        }
+    }
+    else
+    {
+        printf("The matrix must be square.\n");
+    }
+}
