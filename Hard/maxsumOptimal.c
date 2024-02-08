861159b5591c455b191158d399236a10e08f3891
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-02-09 02:03:21 +0530

    modified:   Hard/maxsumOptimal.c

--- Hard/maxsumOptimal.c
+++ Hard/maxsumOptimal.c
@@ -3,15 +3,14 @@
 // Kadane's Algorithm
 void main()
 {
-    // int n;
-    // printf("Enter the number of elements you want in array: ");
-    // scanf("%d",&n);
-    // int arr[n];
-    // for(int i=0;i<n;i++){
-    //     scanf("%d",&arr[i]);
-    // }
-    int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+    printf("Enter the number of elements you want in array: ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    
     int max = INT_MIN, sum = 0, a = 0, b = 0, c = 0;
     for (int i = 0; i < n; i++)
     {
