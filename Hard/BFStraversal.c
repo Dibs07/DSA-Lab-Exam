8ca4cfcf50b570a0756f591b3dcc5b993016ba96
Dibs07 <131288310+Dibs07@users.noreply.github.com>
2024-02-09 01:43:02 +0530

    modified:   Hard/BFStraversal.c

--- Hard/BFStraversal.c
+++ Hard/BFStraversal.c
@@ -54,19 +54,53 @@ void BFS(Graph *g, int s)
     }
 }
 
+void DFSUtil(Graph *g, int v, bool vis[])
+{
+    vis[v] = true;
+    printf("%d ", v);
+
+    for (int i = 0; i < g->val; i++)
+    {
+        if (g->adj[v][i] && !vis[i])
+        {
+            DFSUtil(g, i, vis);
+        }
+    }
+}
+
+void DFS(Graph *g, int s)
+{
+    bool vis[Max];
+    for (int i = 0; i < g->val; i++)
+    {
+        vis[i] = false;
+    }
+
+    DFSUtil(g, s, vis);
+}
+
+
 int main()
 {
 
-    Graph *g = createGraph(4);
-    createEdge(g, 0, 1);
-    createEdge(g, 0, 2);
-    createEdge(g, 1, 2);
-    createEdge(g, 2, 0);
-    createEdge(g, 2, 3);
-    createEdge(g, 3, 3);
+    Graph *g = createGraph(8);
+     createEdge(g,0, 1);    
+        createEdge(g,0, 2);    
+        createEdge(g,0, 3);    
+        createEdge(g,1, 3);    
+        createEdge(g,2, 4);  
+        createEdge(g,3, 5);       
+        createEdge(g,3, 6);    
+        createEdge(g,4, 7);    
+        createEdge(g,4, 5);    
+        createEdge(g,5, 2);    
 
     printf("Breadth First Traversal \n");
     BFS(g, 0);
+    printf("\n");
+
+    printf("Depth First Traversal \n");
+    DFS(g,0);
 
     return 0;
 }
